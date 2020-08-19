73758002297868c9de03fd31f3302ef44cf26a64
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2020-08-19 16:42:05 +0530

    Add files via upload

--- reddit.py
+++ reddit.py
@@ -7,6 +7,9 @@ import time
 import sys
 import argparse
 
+headers = \
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
 
 def get_gfycat_url(gfycat_name):
     response = r.get(gfycat.format(gfycat_name), headers=headers)
